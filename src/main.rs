5c590bc3dfe04569543ca03b9b4017a291bb95e0
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-04-11 00:36:25 +0430

    Remove main.rs

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use rust_randomx::Hasher;
-
-fn main() {
-    let key = b"RandomX example key\x00";
-    let input = b"RandomX example input\x00";
-    let hasher = Hasher::new(key, true);
-    println!("{:?}", hasher.hash(input));
-}
