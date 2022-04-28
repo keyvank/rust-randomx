8055c35b1c259351ce8028b8d345e8cf6eeae831
Amir Ehsandar <ehsandaramir@gmail.com>
2022-04-29 00:47:28 +0430

    Add library support for macos considering cross compiling

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 // build.rs
 
+use std::env;
 use cmake::Config;
 
 fn main() {
@@ -7,5 +8,15 @@ fn main() {
 
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
     println!("cargo:rustc-link-lib=static=randomx");
-    println!("cargo:rustc-link-lib=dylib=stdc++");
+
+    let target_os = env::var("CARGO_CFG_TARGET_OS");
+    match target_os.as_ref().map(|x| &**x) {
+        Ok("linux") | Ok("android") => {
+            println!("cargo:rustc-link-lib=dylib=libc++")
+        }
+        Ok("openbsd") | Ok("bitrig") | Ok("netbsd") | Ok("macos") | Ok("ios") => {
+            println!("cargo:rustc-link-lib=dylib=c++");
+        }
+        tos => panic!("unsupported target os {:?}!", tos)
+    }
 }
