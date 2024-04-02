b67586de8402efed4718154a08774af80b05e955
John Mitrich <johnmitrich@gmail.com>
2024-04-02 23:16:01 +0300

    Update build.rs script to use build directory

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ use std::env;
 fn main() {
     let dst = Config::new("RandomX").define("DARCH", "native").build();
 
-    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-search=native={}/build", dst.display());
     println!("cargo:rustc-link-lib=static=randomx");
 
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or("linux".to_string());
