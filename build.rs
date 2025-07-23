c9019ce9353e0a3f0e437c2b797168e245830180
John Mitrich <75573144+emjomi@users.noreply.github.com>
2025-07-23 19:57:50 +0300

    Fix build script (#7)

--- build.rs
+++ build.rs
@@ -6,18 +6,26 @@ use std::env;
 fn main() {
     let dst = Config::new("RandomX").define("DARCH", "native").build();
 
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
     println!("cargo:rustc-link-search=native={}/build", dst.display());
+
     println!("cargo:rustc-link-lib=static=randomx");
 
-    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or("linux".to_string());
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    let target_env = env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default();
 
-    println!(
-        "cargo:rustc-link-lib=dylib={}",
-        match target_os.as_str() {
-            "openbsd" | "bitrig" | "netbsd" | "macos" | "ios" => {
-                "c++"
-            }
-            _ => "stdc++",
+    match target_os.as_str() {
+        "openbsd" | "bitrig" | "netbsd" | "macos" | "ios" => {
+            println!("cargo:rustc-link-lib=dylib=c++");
         }
-    );
+        "windows" => {
+            println!("cargo:rustc-link-lib=advapi32");
+            if target_env == "gnu" {
+                println!("cargo:rustc-link-lib=dylib=stdc++");
+            }
+        }
+        _ => {
+            println!("cargo:rustc-link-lib=dylib=stdc++");
+        }
+    }
 }
